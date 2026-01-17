5a2f6abfc342e03a01366e6015fbc5bdd52a853e
Mohamed Albakkour <mohamed.albakkour.1378@student.lu.se>
2026-01-17 13:09:37 +0100

    Cors

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,23 @@
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
+import cors from "cors";
+
 
 export function createApp() {
   const app = express();
+
+  app.use(
+    cors({
+      origin: [
+        "http://localhost:5173",
+        "https://travelrouteoptimizer.web.app",
+        "https://travelrouteoptimizer.firebaseapp.com",
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type"],
+    })
+  );
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
@@ -28,6 +42,8 @@ export function createApp() {
 
   return app;
 }
+
+
 
 /**
  * Local development only
